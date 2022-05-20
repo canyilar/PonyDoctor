7ee82d96e58ad090625f472b7a84102768a71384
cenullum <57962685+cenullum@users.noreply.github.com>
2022-05-21 00:31:05 +0300

    buttonlar, ana menu sahnesi, sprite  paketi yüklendi,text mesh eklendi, yeni ui resimleri, font

---
+++ PonyDoctor/Assets/Scripts/MonoSingleton.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType(typeof(T)) as T;
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    obj.name = typeof(T).Name;
+                    instance = obj.AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            gameObject.name= typeof(T).Name;
+            //DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            //Debug.Log(gameObject.name + " " + gameObject.GetInstanceID());/
+            Destroy(gameObject);
+        }
+    }
+}
