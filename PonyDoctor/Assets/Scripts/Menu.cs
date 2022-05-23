221eefdc316044979c6eedbfff8236b8d694872d
cenullum <57962685+cenullum@users.noreply.github.com>
2022-05-23 23:12:19 +0300

    pony ve aletlerin modelleri

--- PonyDoctor/Assets/Scripts/Menu.cs
+++ PonyDoctor/Assets/Scripts/Menu.cs
@@ -2,19 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class Menu : MonoBehaviour
 {
     int wallet = 0;
 
-   public void ChangeScene(string sceneName)
+    [SerializeField]
+    Image flash;
+
+    public void OnEnable()
     {
-        SceneManager.LoadScene(sceneName);
+        flash.color = new Color(1, 1, 1, 1);
+        flash.DOColor(new Color(1, 1, 1, 0), 0.5f);
     }
 
-   public void BuyItem(int itemIndex)
+    public void ChangeScene(string sceneName)
     {
-
+        flash.DOColor(new Color(1, 1, 1, 1), 0.5f).OnComplete (() => SceneManager.LoadScene(sceneName));
     }
+
+
+ 
 
 }
